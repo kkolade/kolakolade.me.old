7b68c56231b3b676e38b6d6b3ce2f60dfae6db6e
belaymit <2belamit@gmail.com>
2022-11-14 21:12:15 +0300

    added class dynamically to mebile menu items

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 // Variabless
-const hamburger = document.querySelector(".main-nav__hamburger"),
-  menuList = document.querySelector(".main-nav__list");
+
+const hamburger = document.querySelector('.main-nav__hamburger');
+// eslint-disable-next-line no-unused-vars
+const menuList = document.querySelector('.main-nav__list');
+const mainHeader = document.querySelector('.main-header__container');
+
+// eslint-disable-next-line no-console
+console.log(hamburger);
+// eslint-disable-next-line no-console
+console.log(menuList);
 
 // Event Listeners
-hamburger.addEventListener("click", () => {
-  hamburger.classList.add("show-element");
+hamburger.addEventListener('click', () => {
+  menuList.classList.toggle('show-element');
+  mainHeader.classList.add('main-header_toggle');
 });

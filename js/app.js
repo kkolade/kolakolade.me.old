7491e42b1e0f1f2cb79396e55f825b6ea62e8612
Kola Kolade <kolakolade@gmail.com>
2022-11-15 13:31:27 +0100

    Implemented project requirement 1
    When the user clicks (or taps) the hamburger button, the mobile menu appears.
    Renamed html element required for the toggle action to have semantic meaning
    Selected relevant elements in JS and added events and other action

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
 // Variabless
 
-const hamburger = document.querySelector('.main-nav__hamburger');
-// eslint-disable-next-line no-unused-vars
-const menuList = document.querySelector('.main-nav__list');
-const mainHeader = document.querySelector('.main-header__container');
+const toggleIcons = document.querySelector(".main-nav__toggle-icons");
+const menuList = document.querySelector(".main-nav__list");
+const mainHeader = document.querySelector(".main-header__container");
+const iconBars = document.querySelector(".fa-bars");
+const iconTimes = document.querySelector(".fa-times");
 
 // eslint-disable-next-line no-console
-console.log(hamburger);
+console.log(toggleIcons);
 // eslint-disable-next-line no-console
 console.log(menuList);
+console.log(iconBars);
+console.log(iconTimes);
 
 // Event Listeners
-hamburger.addEventListener('click', () => {
-  menuList.classList.toggle('show-element');
-  mainHeader.classList.add('main-header_toggle');
+// toggleIcons.addEventListener("click", () => {
+//   menuList.classList.toggle("show-element");
+//   mainHeader.classList.add("main-header_toggle");
+// });
+
+toggleIcons.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target.classList.contains("fa-bars")) {
+    iconBars.classList.toggle("hide-element");
+    iconTimes.classList.toggle("hide-element");
+    iconTimes.classList.toggle("on-top");
+    menuList.classList.toggle("show-element");
+    mainHeader.classList.add("main-header_toggle");
+    console.log(e.target);
+  } else if (e.target.classList.contains("fa-times")) {
+    iconBars.classList.toggle("hide-element");
+    iconTimes.classList.toggle("hide-element");
+    menuList.classList.toggle("show-element");
+    mainHeader.classList.add("main-header_toggle");
+    console.log(e.target);
+  }
 });

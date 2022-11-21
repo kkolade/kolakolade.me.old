2c2a344d9e795db7979ed4e305da44a51c7ca206
Gabriel Cilfone <79432268+cilfonegabriel@users.noreply.github.com>
2022-11-21 17:34:40 -0300

    add var to js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,13 @@ const menuList = document.querySelector(".main-nav__list");
 const mainHeader = document.querySelector(".main-header__container");
 const iconBars = document.querySelector(".fa-bars");
 const iconTimes = document.querySelector(".fa-times");
+
+toggleIcons.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target.classList.contains("fa-bars")) {
+    iconBars.classList.toggle("hide-element");
+    iconTimes.classList.toggle("hide-element");
+    iconTimes.classList.toggle("on-top");
+    menuList.classList.toggle("show-element");
+    mainHeader.classList.add("main-header_toggle");
+  }

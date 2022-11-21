5cb705a4435ad55cde776808027981e81cbd92c5
Kola Kolade <kolakolade@gmail.com>
2022-11-21 22:43:27 +0100

    Fixed conflict of redeclaring const in the JS file

--- js/app.js
+++ js/app.js
@@ -7,16 +7,6 @@ const mainHeader = document.querySelector(".main-header__container");
 const iconBars = document.querySelector(".fa-bars");
 const iconTimes = document.querySelector(".fa-times");
 
-
-// MOBILE MENU
-// Variabless
-
-const toggleIcons = document.querySelector(".main-nav__toggle-icons");
-const menuList = document.querySelector(".main-nav__list");
-const mainHeader = document.querySelector(".main-header__container");
-const iconBars = document.querySelector(".fa-bars");
-const iconTimes = document.querySelector(".fa-times");
-
 toggleIcons.addEventListener("click", (e) => {
   e.stopPropagation();
   if (e.target.classList.contains("fa-bars")) {

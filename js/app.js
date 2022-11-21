836330469e46d142cb7eae92252b91ca1a6cb942
Kola Kolade <kolakolade@gmail.com>
2022-11-21 21:48:15 +0100

    Created branch to implement the mobile menu

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// MOBILE MENU
+// Variabless
+
+const toggleIcons = document.querySelector(".main-nav__toggle-icons");
+const menuList = document.querySelector(".main-nav__list");
+const mainHeader = document.querySelector(".main-header__container");
+const iconBars = document.querySelector(".fa-bars");
+const iconTimes = document.querySelector(".fa-times");
+
+
+
